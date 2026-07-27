65b0f398bc4183d0be54f48e569dd881e2779df7
Om Maurya <ommaurya2580@gmail.com>
2026-07-27 20:08:58 +0530

    fix(monorepo): configure distDir, outputFileTracingRoot and transpilePackages for Vercel monorepo deployment

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: [
+    "three",
+    "@react-three/fiber",
+    "@react-three/drei",
+    "@react-spring/three",
+    "@react-three/postprocessing",
+    "gsap",
+    "zustand"
+  ],
+  distDir: "../../.next",
+  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 export default nextConfig;
